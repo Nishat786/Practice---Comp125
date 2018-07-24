53c97d624a2ef5c0bca15be6fc760f90486a26dd
NishatBagga <bagganishat111@gmail.com>
2018-07-24 19:29:00 -0400

    Added to Button

--- Scripts/App.js
+++ Scripts/App.js
@@ -8,6 +8,23 @@
 
         let title = document.title;
 
+        switch(title) {
+
+            case "My WEBPAGE! and DEVELOPMENT!":
+                let AboutButton = document.getElementById("AboutButton");
+
+                AboutButton.addEventListener("click", function(){
+                    console.log("About Button was Clicked!");
+                });
+            break;
+
+            case "My Webpage - About!":
+            break;
+
+            case "Contact Us":
+            break;
+        }        
+
         console.log("App Started!");
         console.log("------------------");
         console.log("Title: "+ title);
