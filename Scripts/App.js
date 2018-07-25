8aea4d48ac751d4434a3f348818a136e6b0daf8d
NishatBagga <bagganishat111@gmail.com>
2018-07-24 23:44:30 -0400

    Added functions and data

--- Scripts/App.js
+++ Scripts/App.js
@@ -3,27 +3,57 @@
 // IIFE - Immediately Invoked Function Expression
 // anonyms self execuing funtion 
 (function(){
+        "Use Strict"
+
+        function AboutButtonClick() {
+            console.log("About Button was Clicked!");
+        }
+
+        function AboutButtonOver(event) {
+            event.currentTarget.style.opacity = 0.3;
+        }
+
+        function AboutButtonOut() {
+            event.currentTarget.style.opacity = 1.0;
+        }
+
+    function HomeContent() {
+        let AboutButton = document.getElementById("AboutButton");
+
+        AboutButton.innerText = "About";
+
+            AboutButton.addEventListener("click", AboutButtonClick);
+            AboutButton.addEventListener("mouseover", AboutButtonOver);
+            AboutButton.addEventListener("mouseout", AboutButtonOut);
+
+    }
+
+    function AboutContent() {
+        let Paragraph = document.getElementById("Paragraph");
+
+        Paragraph.textContent = "We will be Changing the Contents of this Page.";
+    }
 
     function Start() {
-
+        // local variable
         let title = document.title;
 
         switch(title) {
+            case "My Webpage - Home!":            
+                HomeContent();
+                break;
 
-            case "My WEBPAGE! and DEVELOPMENT!":
-                let AboutButton = document.getElementById("AboutButton");
+                case "My Webpage - About!":
+                AboutContent();
+                break;
 
-                AboutButton.addEventListener("click", function(){
-                    console.log("About Button was Clicked!");
-                });
-            break;
+                case "Contact Us":
+                break;
 
-            case "My Webpage - About!":
-            break;
-
-            case "Contact Us":
-            break;
-        }        
+                default:
+                break;
+        }
+                     
 
         console.log("App Started!");
         console.log("------------------");
